bf144bedb0ff6abf63bec2704f3e8f4b622e38c4
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-06 18:49:03 +0700

    mengisi data

--- getline.cpp
+++ getline.cpp
@@ -11,3 +11,19 @@ struct Orang
     detailAlamat alamat;
     int umur;
 };
+
+int main () {
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : " ;
+    getline(cin, mhs.nama);
+    cout << "desa : " ;
+    getline(cin, mhs.alamat.desa);
+    cout << "kota : " ;
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+   
+}

d141da1a2ca53c1f80d9a50ec578cd273d56e214
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-06 18:41:47 +0700

    struck alamat dan orang

---
+++ structnew.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+struct Orang 
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};

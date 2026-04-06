1a25af70a521f1cff98a3fea3083c644b086acb0
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-06 18:28:28 +0700

    main dan mengisi data

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,16 @@ struct orang
     string alamat;
     int umur;
 };
+
+int main () {
+    orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : " ;
+    cin >> mhs.nama;
+    cout << "Alamat : " ;
+    cin >> mhs.alamat;
+    cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+}

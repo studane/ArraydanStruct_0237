c31623a08b08d88a61e82a2096483470850c9ade
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-06 17:27:23 +0700

    deklarasi dengan inisialisasi

---
+++ array.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+//Nama Repo : ArraydanStruct_4NIMBLKNG
+//Minimal commit : 15 commit
+int main() {
+    // deklarasi dengan inisialisasi
+    int aku[5] = {10,20,30,40,50};
+}

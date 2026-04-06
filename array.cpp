a5f8a6b5c8b8a29753490d04083b83961aeb4302
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-06 17:37:18 +0700

    menampilkan satu per satu

--- array.cpp
+++ array.cpp
@@ -25,4 +25,11 @@ int main() {
     cout << "Data index 4 = ";
     cin >> kamu[4];
     cout << endl;
+    // Menampilkan satu per satu
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data Kedua = " << kamu[1] << endl;
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu [3] << endl;
+    cout << "Data Kelima = " << kamu [4] << endl;
+    cout << endl;
 }

ba1e65ecb1711a304000767d1df3b242bcb4868c
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-06 18:56:30 +0700

    struct alamat dan orang

---
+++ arrayStruct.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    string kota;;
+};
+
+struct Orang
+{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};

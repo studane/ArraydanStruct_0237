c2607e978bf155c83a9bb3bcf215e94defec0c60
studane <zhidaneprabowoguzlend@gmail.com>
2026-04-06 18:59:03 +0700

    main dan mengisi data

--- arrayStruct.cpp
+++ arrayStruct.cpp
@@ -13,3 +13,24 @@ struct Orang
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Orang mhs[3];
+    
+    for (int i = 0; i <= 2; i++)
+    {
+
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    getline(cin ,  mhs[i].nama);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.desa);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs[i].umur;
+    cout << endl;
+
+    }
+}
